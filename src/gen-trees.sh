7b9bd543a8e20f5a04dabd11cfcdfc5c3d9ee928
Mahesh Jethanandani <mjethanandani@gmail.com>
2018-02-11 13:02:43 -0800

    Restored macros and added Al's changes

--- src/gen-trees.sh
+++ src/gen-trees.sh
@@ -1,6 +1,13 @@
-
-pyang -p ../ -f tree yang/ietf-twamp\@*.yang > yang/ietf-twamp-tree.txt.tmp
-
-fold -w 71 yang/ietf-twamp-tree.txt.tmp > yang/ietf-twamp-tree.txt
-
+for i in yang/*\@$(date +%Y-%m-%d).yang
+do
+    name=$(echo $i | cut -f 1 -d '.')
+    echo $name
+    if test "${name#^example}" = "$name"; then
+        pyang -p ../ -f tree --tree-line-length=69 $name.yang > $name-tree.txt.tmp
+    else            
+        pyang --ietf -p ../ -f tree --ietf --tree-line-length=69 $name.yang > $name-tree.txt.tmp
+    fi
+    fold -w 71 $name-tree.txt.tmp > $name-tree.txt
+    yanglint -p ../../ $name.yang
+done
 rm yang/*-tree.txt.tmp
