dcfabe827f97a22a3112823ff6d2cd6220747597
Mahesh Jethanandani <mjethanandani@gmail.com>
2018-04-05 15:36:20 -0700

    Changes to address AD comments.

--- src/insert-figures.sh
+++ src/insert-figures.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 # make sure input params are good
 if [ "$#" == "0" ]; then
-    echo "Usage: $0 file"
+    echo "Usage: $0 file[,fold-length]"
     exit
 fi
 
@@ -15,7 +15,20 @@ while ( grep INSERT_TEXT_FROM_FILE .tmp.new.txt >> /dev/null ); do
     awk "NR<$linenum" .tmp.new.txt > .tmp.pre.txt
     awk "NR>$linenum" .tmp.new.txt > .tmp.post.txt
 
-    cat .tmp.pre.txt $file .tmp.post.txt > .tmp.new.txt
+    if [ `echo $file | grep ","` ]; then
+      col=`echo $file | sed 's/.*,//'`
+      file=`echo $file | sed 's/,.*//'`
+      file2=$file.2
+      echo "[note: '\' line wrapping is for formatting only]" > $file2
+      echo "" >> $file2
+      gsed "s/\(.\{$col\}\)/\1\\\\\n/" < $file >> $file2
+      cat .tmp.pre.txt $file2 .tmp.post.txt > .tmp.new.txt
+      rm $file2
+    else
+      cat .tmp.pre.txt $file .tmp.post.txt > .tmp.new.txt
+    fi
+
+
 done
 
 cat .tmp.new.txt
